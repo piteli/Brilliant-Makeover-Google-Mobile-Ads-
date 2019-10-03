d1292ce7935a6f6fa5f8e46555ef33eb9d03524a
piteli <rushwender@gmail.com>
2019-10-04 07:55:30 +0800

    added banner view with live content

--- brilliant-makeover/ViewController.swift
+++ brilliant-makeover/ViewController.swift
@@ -7,11 +7,17 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bannerView: GADBannerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.rootViewController = self
+        bannerView.load(GADRequest())
         // Do any additional setup after loading the view, typically from a nib.
     }
 
