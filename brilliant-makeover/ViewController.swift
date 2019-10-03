08cb01552d6431b1abc6128e235fad25f0496367
piteli <rushwender@gmail.com>
2019-10-04 07:30:01 +0800

    Initial Commit

---
+++ brilliant-makeover/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  brilliant-makeover
+//
+//  Created by Fitri San on 04/10/2019.
+//  Copyright © 2019 Fitri San. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
